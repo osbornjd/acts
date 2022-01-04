6991c557fe9796d218f23ac056eb7e1fff9ade3d
Joe Osborn <osbornjd@ornl.gov>
2022-01-04 17:00:55 -0500

    fixup fitting algo header

--- Examples/Algorithms/TrackFitting/include/ActsExamples/TrackFitting/TrkrClusterFittingAlgorithm.hpp
+++ Examples/Algorithms/TrackFitting/include/ActsExamples/TrackFitting/TrkrClusterFittingAlgorithm.hpp
@@ -6,6 +6,7 @@
 #include <stdexcept>
 #include <boost/program_options.hpp>
 
+#include "Acts/Definitions/TrackParametrization.hpp"
 #include "Acts/TrackFitting/GainMatrixSmoother.hpp"
 #include "Acts/TrackFitting/GainMatrixUpdater.hpp"
 #include "Acts/Geometry/GeometryIdentifier.hpp"
@@ -17,14 +18,13 @@
 #include "Acts/Propagator/Propagator.hpp"
 #include "Acts/Surfaces/Surface.hpp"
 #include "Acts/Utilities/Helpers.hpp"
-#include "Acts/Utilities/ParameterDefinitions.hpp"
 #include "Acts/TrackFitting/KalmanFitter.hpp"
 #include "Acts/Geometry/TrackingGeometry.hpp"
 
-#include "ActsExamples/Plugins/BField/ScalableBField.hpp"
+#include "ActsExamples/MagneticField/ScalableBField.hpp"
 #include "ActsExamples/EventData/Track.hpp"
 #include "ActsExamples/Framework/BareAlgorithm.hpp"
-#include "ActsExamples/Plugins/BField/BFieldOptions.hpp"
+#include "ActsExamples/MagneticField/MagneticFieldOptions.hpp"
 #include "ActsExamples/EventData/TrkrClusterSourceLink.hpp"
 
 namespace ActsExamples {
@@ -33,46 +33,76 @@ namespace ActsExamples {
  * This class contains the information required to run the Kalman fitter
  * with the TrkrClusterSourceLinks. Based on ActsExamples::FittingAlgorithm
  */
-class TrkrClusterFittingAlgorithm : public BareAlgorithm
+class TrkrClusterFittingAlgorithm final : public BareAlgorithm
 {
  public:
   /// Construct some aliases to be used for the fitting results
+  using TrackFitterOptions = Acts::KalmanFitterOptions;
   using FitterResult
-    = Acts::Result<Acts::KalmanFitterResult<ActsExamples::TrkrClusterSourceLink>>;
-  using FitterFunction
-    = std::function<FitterResult(
-		    const std::vector<ActsExamples::TrkrClusterSourceLink>&,
-		    const TrackParameters&,
-		    const Acts::KalmanFitterOptions<Acts::VoidOutlierFinder>&)>;
-
-  using DirectedFitterFunction
-  = std::function<FitterResult(
-		    const std::vector<ActsExamples::TrkrClusterSourceLink>&,
-		    const TrackParameters&,
-		    const Acts::KalmanFitterOptions<Acts::VoidOutlierFinder>&,
-		    const std::vector<const Acts::Surface*>&)>;
-      
-
-  /// Create fitter function
-  static FitterFunction makeFitterFunction(
-      std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry,
-      Options::BFieldVariant magneticField);
-
-  static DirectedFitterFunction makeFitterFunction(
-      Options::BFieldVariant magneticField);
-
-  struct Config
-  {
-    FitterFunction fit;
-    DirectedFitterFunction dFit;
+    = Acts::Result<Acts::KalmanFitterResult>;
+  
+  class TrackFitterFunction {
+    public:
+      virtual ~TrackFitterFunction() = default;
+      virtual FitterResult operator()(
+         const std::vector<std::reference_wrapper<const TrkrClusterSourceLink>>&,
+       	 const TrackParameters&, const TrackFitterOptions&) const = 0;
   };
 
+  class DirectedTrackFitterFunction {
+    public:
+      virtual ~DirectedTrackFitterFunction() = default;
+      virtual FitterResult operator()(
+          const std::vector<std::reference_wrapper<const TrkrClusterSourceLink>>&,
+          const TrackParameters&, const TrackFitterOptions&,
+          const std::vector<const Acts::Surface*>&) const = 0;
+  };
+
+  static std::shared_ptr<TrackFitterFunction> makeTrackFitterFunction(
+      std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry,
+      std::shared_ptr<const Acts::MagneticFieldProvider> magneticField);
+
+  static std::shared_ptr<DirectedTrackFitterFunction> makeTrackFitterFunction(
+      std::shared_ptr<const Acts::MagneticFieldProvider> magneticField);
+
+
+  struct Config {
+    bool directNavigation;
+    std::shared_ptr<TrackFitterFunction> fit;
+    std::shared_ptr<DirectedTrackFitterFunction> dfit;
+    std::shared_ptr<const Acts::TrackingGeometry> tGeometry;
+    bool multipleScattering = true;
+    bool energyLoss = true;
+  };
   /// Constructor 
   TrkrClusterFittingAlgorithm(Config cfg, Acts::Logging::Level lvl);
 
 
-private:
+ private:
   Config m_cfg;
+
+  /// Helper function to call correct FitterFunction
+  FitterResult fitTrack(
+      const std::vector<std::reference_wrapper<
+          const ActsExamples::TrkrClusterSourceLink>>& sourceLinks,
+      const ActsExamples::TrackParameters& initialParameters,
+      const TrackFitterOptions& options,
+      const std::vector<const Acts::Surface*>& surfSequence) const;
+
 };
+
+inline ActsExamples::TrkrClusterFittingAlgorithm::FitterResult
+ActsExamples::TrkrClusterFittingAlgorithm::fitTrack(
+    const std::vector<std::reference_wrapper<
+        const ActsExamples::TrkrClusterSourceLink>>& sourceLinks,
+    const ActsExamples::TrackParameters& initialParameters,
+    const Acts::KalmanFitterOptions& options,
+    const std::vector<const Acts::Surface*>& surfSequence) const {
+  if (m_cfg.directNavigation) {
+    return (*m_cfg.dfit)(sourceLinks, initialParameters, options, surfSequence);
+  }
+
+  return (*m_cfg.fit)(sourceLinks, initialParameters, options);
+}
 
 }
