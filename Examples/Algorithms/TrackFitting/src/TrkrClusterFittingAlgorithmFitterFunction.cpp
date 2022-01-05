cbde70f84f10d41bb7efd738287e33e6da6c6862
Joe Osborn <osbornjd@ornl.gov>
2022-01-05 08:50:09 -0500

    fix fitter function

--- Examples/Algorithms/TrackFitting/src/TrkrClusterFittingAlgorithmFitterFunction.cpp
+++ Examples/Algorithms/TrackFitting/src/TrkrClusterFittingAlgorithmFitterFunction.cpp
@@ -1,10 +1,11 @@
-#include "ActsExamples/Fitting/TrkrClusterFittingAlgorithm.hpp"
+#include "ActsExamples/TrackFitting/TrkrClusterFittingAlgorithm.hpp"
 
 #include <iostream>
 #include <map>
 #include <random>
 #include <stdexcept>
 
+#include "Acts/Definitions/TrackParametrization.hpp"
 #include "Acts/TrackFitting/GainMatrixSmoother.hpp"
 #include "Acts/TrackFitting/GainMatrixUpdater.hpp"
 #include "Acts/Geometry/GeometryIdentifier.hpp"
@@ -16,46 +17,56 @@
 #include "Acts/Propagator/Propagator.hpp"
 #include "Acts/Surfaces/Surface.hpp"
 #include "Acts/Utilities/Helpers.hpp"
-#include "Acts/Utilities/ParameterDefinitions.hpp"
 #include "boost/program_options.hpp"
 
-#include "ActsExamples/Plugins/BField/ScalableBField.hpp"
+#include "ActsExamples/MagneticField/ScalableBField.hpp"
 
 
 /**
  * Struct that calls the fitting algorithm to get the result of the fit
  */
 namespace {
-template <typename Fitter>
+
+using Updater = Acts::GainMatrixUpdater;
+using Smoother = Acts::GainMatrixSmoother;
+using Stepper = Acts::EigenStepper<>;
+using Propagator = Acts::Propagator<Stepper, Acts::Navigator>;
+using Fitter = Acts::KalmanFitter<Propagator>;
+using DirectPropagator = Acts::Propagator<Stepper, Acts::DirectNavigator>;
+using DirectFitter = Acts::KalmanFitter<DirectPropagator>;
+
 struct TrkrFitterFunctionImpl
-{
+    : public ActsExamples::TrkrClusterFittingAlgorithm::TrackFitterFunction {
   Fitter fitter;
 
   TrkrFitterFunctionImpl(Fitter&& f) : fitter(std::move(f)) {}
 
   ActsExamples::TrkrClusterFittingAlgorithm::FitterResult operator()(
-       const std::vector<ActsExamples::TrkrClusterSourceLink>& sourceLinks,
-       const ActsExamples::TrackParameters& initialParameters,
-       const Acts::KalmanFitterOptions<Acts::VoidOutlierFinder>& options) const
-  {
-    return fitter.fit(sourceLinks, initialParameters, options);
+      const std::vector<std::reference_wrapper<
+          const ActsExamples::TrkrClusterSourceLink>>& sourceLinks,
+      const ActsExamples::TrackParameters& initialParameters,
+      const ActsExamples::TrkrClusterFittingAlgorithm::TrackFitterOptions& options)
+      const  {
+    return fitter.fit(sourceLinks.begin(), sourceLinks.end(),
+		      initialParameters, options);
   };
 };
 
-template <typename Fitter>
 struct DirectedTrkrFitterFunctionImpl
+  : public ActsExamples::TrkrClusterFittingAlgorithm::DirectedTrackFitterFunction 
 {
-  Fitter fitter;
-
-  DirectedTrkrFitterFunctionImpl(Fitter&& f) : fitter(std::move(f)) {}
+  DirectFitter fitter;
+  DirectedTrkrFitterFunctionImpl(DirectFitter&& f) : fitter(std::move(f)) {}
 
   ActsExamples::TrkrClusterFittingAlgorithm::FitterResult operator()(
-       const std::vector<ActsExamples::TrkrClusterSourceLink>& sourceLinks,
-       const ActsExamples::TrackParameters& initialParameters,
-       const Acts::KalmanFitterOptions<Acts::VoidOutlierFinder>& options,
-       const std::vector<const Acts::Surface*>& sSequence) const
+	  const std::vector<std::reference_wrapper<
+	  const ActsExamples::TrkrClusterSourceLink>>& sourceLinks,
+	  const ActsExamples::TrackParameters& initialParameters,
+	  const ActsExamples::TrkrClusterFittingAlgorithm::TrackFitterOptions& options,
+	  const std::vector<const Acts::Surface*>& sSequence) const
   {
-    return fitter.fit(sourceLinks, initialParameters, options, sSequence);
+    return fitter.fit(sourceLinks.begin(), sourceLinks.end(), 
+		      initialParameters, options, sSequence);
   };
 };
 
@@ -64,69 +75,34 @@ struct DirectedTrkrFitterFunctionImpl
 /**
  * Function that actually makes the fitting function to be used 
  */
-ActsExamples::TrkrClusterFittingAlgorithm::FitterFunction
-ActsExamples::TrkrClusterFittingAlgorithm::makeFitterFunction(
+
+std::shared_ptr<ActsExamples::TrkrClusterFittingAlgorithm::TrackFitterFunction>
+ActsExamples::TrkrClusterFittingAlgorithm::makeTrackFitterFunction(
     std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry,
-    Options::BFieldVariant magneticField)
-{
-  using Updater  = Acts::GainMatrixUpdater;
-  using Smoother = Acts::GainMatrixSmoother;
+    std::shared_ptr<const Acts::MagneticFieldProvider> magneticField) {
+  Stepper stepper(std::move(magneticField));
+  Acts::Navigator::Config cfg{trackingGeometry};
+  cfg.resolvePassive = false;
+  cfg.resolveMaterial = true;
+  cfg.resolveSensitive = true;
+  Acts::Navigator navigator(cfg);
+  Propagator propagator(std::move(stepper), std::move(navigator));
+  Fitter trackFitter(std::move(propagator));
 
-  /// Return a new instance of the fitter
-  return std::visit(
-      [trackingGeometry](auto&& inputField) -> FitterFunction {
-	/// Construct some aliases for the components below
-        using InputMagneticField = typename std::decay_t<decltype(inputField)>::element_type;
-        using MagneticField      = Acts::SharedBField<InputMagneticField>;
-        using Stepper            = Acts::EigenStepper<MagneticField>;
-        using Navigator          = Acts::Navigator;
-        using Propagator         = Acts::Propagator<Stepper, Navigator>;
-        using Fitter             = Acts::KalmanFitter<Propagator, Updater, Smoother>;
-
-        /// Make the components for the fitter
-        MagneticField field(std::move(inputField));
-        Stepper       stepper(std::move(field));
-        Navigator     navigator(trackingGeometry);
-        navigator.resolvePassive   = false;
-        navigator.resolveMaterial  = true;
-        navigator.resolveSensitive = true;
-        Propagator propagator(std::move(stepper), std::move(navigator));
-        Fitter     fitter(std::move(propagator));
-			 
-        /// Build the fitter function
-        return TrkrFitterFunctionImpl<Fitter>(std::move(fitter));
-      },
-      std::move(magneticField));
+  // build the fitter functions. owns the fitter object.
+  return std::make_shared<TrkrFitterFunctionImpl >(std::move(trackFitter));
 }
 
+std::shared_ptr<
+    ActsExamples::TrkrClusterFittingAlgorithm::DirectedTrackFitterFunction>
+ActsExamples::TrkrClusterFittingAlgorithm::makeTrackFitterFunction(
+    std::shared_ptr<const Acts::MagneticFieldProvider> magneticField) {
+  // construct all components for the fitter
+  Stepper stepper(std::move(magneticField));
+  Acts::DirectNavigator navigator;
+  DirectPropagator propagator(std::move(stepper), std::move(navigator));
+  DirectFitter fitter(std::move(propagator));
 
-ActsExamples::TrkrClusterFittingAlgorithm::DirectedFitterFunction
-ActsExamples::TrkrClusterFittingAlgorithm::makeFitterFunction(
-    Options::BFieldVariant magneticField)
-{
-  using Updater  = Acts::GainMatrixUpdater;
-  using Smoother = Acts::GainMatrixSmoother;
-
-  /// Return a new instance of the fitter
-  return std::visit(
-      [](auto&& inputField) -> DirectedFitterFunction {
-	/// Construct some aliases for the components below
-        using InputMagneticField = typename std::decay_t<decltype(inputField)>::element_type;
-        using MagneticField      = Acts::SharedBField<InputMagneticField>;
-        using Stepper            = Acts::EigenStepper<MagneticField>;
-        using Navigator          = Acts::DirectNavigator;
-        using Propagator         = Acts::Propagator<Stepper, Navigator>;
-        using Fitter             = Acts::KalmanFitter<Propagator, Updater, Smoother>;
-
-        /// Make the components for the fitter
-        MagneticField field(std::move(inputField));
-        Stepper       stepper(std::move(field));
-        Navigator     navigator;
-        Propagator propagator(std::move(stepper), std::move(navigator));
-        Fitter     fitter(std::move(propagator));
-			 
-        /// Build the fitter function
-        return DirectedTrkrFitterFunctionImpl<Fitter>(std::move(fitter));
-      },
-      std::move(magneticField));
+  // build the fitter functions. owns the fitter object.
+  return std::make_shared<DirectedTrkrFitterFunctionImpl>(std::move(fitter));
 }
